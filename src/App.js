d33baaad9ebc205081d254e939fb7889e9e606a1
vertex <pro.omarjribi@gmail.com>
2019-08-15 13:18:36 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './style.css'
+import srcImage from './imageInSrc.jpg'
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{border:'solid 1px black' , maxWidth:'100vw'}}>
+
+      <h1 className={"title red"}>My title</h1>
+      
+      <br />
+
+      <img src={srcImage} alt='srcImg' />
+
+      <br />
+
+      <img src="/imageInPublic.png" alt='pubImage' />
+
     </div>
   );
 }
